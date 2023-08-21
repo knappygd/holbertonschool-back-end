5484f310afeb8593291aef97733325a5d9965f6e
Froggstar11 <migueldamasco111@gmail.com>
2023-08-21 12:23:32 -0700

    added 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Gather user's todo from API."""
+import requests as r
+import sys
+import json
+
+
+if __name__ == "__main__":
+    id = int(sys.argv[1])
+    user = r.get(f'https://jsonplaceholder.typicode.com/users/{id}').json()
+    todo = r.get('https://jsonplaceholder.typicode.com/todos').json()
+    userlist = []
+
+    with open("{}.json".format(id), "w") as file:
+        for t in todo:
+            if t["userId"] == id:
+                userlist.append({
+                    "task": t['title'],
+                    "completed": t['completed'],
+                    "username": user["username"]
+                })
+        json.dump({str(id): userlist}, file)
